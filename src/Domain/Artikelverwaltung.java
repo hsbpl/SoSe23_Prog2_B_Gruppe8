41f84913fe63510e1feee4fa5a10b3647b20f9c2
RohaAhmad <rahmad@stud.hs-bremen.de>
2023-04-30 22:26:42 +0200

    mit Artikelverwaltung angefangen

--- src/Domain/Artikelverwaltung.java
+++ src/Domain/Artikelverwaltung.java
@@ -2,6 +2,21 @@ package Domain;
 
 import ValueObjekt.Artikel;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Artikelverwaltung {
-    private List<Artikel> waren = new ArrayList<>();
+    private List<Artikel> waren;
+
+    public Artikelverwaltung() {
+        waren = new ArrayList<>();
+    }
+
+    public void artikelAnlegen(Artikel artikel){
+        artikelListe.add(artikel);
+    }
+    public void
 }
+
