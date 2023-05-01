76022c1a2921e003ab4c5f4bbecdb0086065ea64
sranahsbremen <131173369+sranahsbremen@users.noreply.github.com>
2023-05-01 19:33:12 +0200

    Kundenverwaltung in Domain Methoden um Registrierung Erweitert (noch nicht ganz fertig).

--- src/Domain/Kundenverwaltung.java
+++ src/Domain/Kundenverwaltung.java
@@ -1,6 +1,8 @@
 package Domain;
 
 import ValueObjekt.Artikel;
+import ValueObjekt.User;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -8,13 +10,19 @@ import java.util.List;
 
 public class Kundenverwaltung {
 private List<Artikel> meinWarenkorb = new ArrayList();
+private List<User> kRegistrierung = new ArrayList();
 
     public Kundenverwaltung(List<Artikel> meinWarenkorb){
         this.meinWarenkorb = meinWarenkorb;
+        this.kRegistrierung = kRegistrierung;
     }
 
     public List<Artikel> getMeinWarenkorb() {
         return meinWarenkorb;
+    }
+
+    public List<User> getKRegistrierung() {
+        return kRegistrierung;
     }
 
     /* Kunde legt gewünschte Menge an Artikeln in den Warenkorb, sofern sie vorhanden sind */
@@ -23,6 +31,8 @@ private List<Artikel> meinWarenkorb = new ArrayList();
         *
         * Iterator<Artikel> checkArtikelverwaltung = Artikelverwaltung.iterator;??
         * muss ich noch Fertigstellen
+        * ---> muss irgendwie auf die Artikelverwaltung zugreifen um sicher zu gehen,
+        * das die Artikel aus dem Betand sind
         * */
         for (int i = 0; i < menge; i++) {
             while (artikel.inStock()) {
@@ -33,11 +43,23 @@ private List<Artikel> meinWarenkorb = new ArrayList();
     }
 
     /* Warenkorb wird geleert*/
-    public List<Artikel> leeren (){
+    public void leeren (){
         meinWarenkorb.clear();
-        return meinWarenkorb;
     }
-
+    /*Es wird überprüft ob das Konto bereits existiert, Kunden können sich registrieren
+    * Aber mein Kopf hat grad einen Knoten schau nochmal morgen rein welche for/if Reihenfolge Sinn macht
+    * */
+    public String registrierung(User neu){
+        for (User u: kRegistrierung){
+            if (u.equals(neu)){
+                return "Das Konto existiert bereits.";
+            }
+        }
+            kRegistrierung.add(neu);
+            return "Herzlichen glückwusch zu deinem Eshop Account";
+        }
 
 }
+
+
 
