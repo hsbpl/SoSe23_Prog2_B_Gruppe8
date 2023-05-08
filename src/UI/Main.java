f749aba36679bcbd208d53f415db149810d65b0f
RohaAhmad <rahmad@stud.hs-bremen.de>
2023-05-08 23:51:57 +0200

    Habe Artikelverwalttung soweit fertig und restliche fehler behoben muss evtl. nur noch einmal public void artikelLöschen hinzufügen für den scanner.
    Hab ordner Artikel etwas übersichtlicher gemacht.
    
    und in der main angefangen eine Benutzungsschnittstelle zu erstellen um artikelSortierenNachBezeichnung und artikelSortierenNachArtikelNummer ausgeben zu können. ist noch nicht fertig, weil ich euch noch fragen wollte.

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -1,11 +1,51 @@
 package UI;
 
+import Domain.Artikelverwaltung;
 import Domain.Kundenverwaltung;
 import Domain.Lagerverwaltung;
 import ValueObjekt.Artikel;
 import ValueObjekt.Mitarbeiter;
 
 import java.util.Scanner;
+public class Benutzungsschnittstelle{
+    private Artikelverwaltung artikelverwaltung;
+
+    public Benutzungsschnittstelle(){
+        artikelverwaltung = new Artikelverwaltung();
+    }
+    public void start(){
+        Scanner scanner = new Scanner(System.in);
+        int auswahl = 0;
+
+        while (auswahl != 6){
+            System.out.println("Artikelverwaltung");
+            System.out.println("1. Artikel hinzufügen");
+            System.out.println("2. Artikel löschen");
+            System.out.println("3. Artikel ausgeben");
+            System.out.println("4. Nach Beziehung sortieren");
+            System.out.println("5. Nach Artikelnummer sortieren");
+            System.out.println("6. Beenden");
+            System.out.println("Auswahl: ");
+            auswahl =scanner.nextInt();
+
+            switch (auswahl){
+                case 1:
+                    artikelverwaltung.artikelHinzufuegen();
+                    break;
+                case 2:
+                    artikelLoeschen(scanner);
+                    break;
+                case 3:
+
+            }
+
+
+        }
+    }
+// private void loeschen behalte ich erstmal so lange bis ich in artikelverwaltung artikelloeschen erstellt habe.
+    private void artikelLoeschen(Scanner scanner) {
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
