391f84f604591536ff36461397f66a89800ef199
kemicha <chakounangkevine@yahoo.fr>
2023-05-10 13:50:34 +0200

    Mitarbeiterverwaltung

--- src/Domain/Mitarbeiterverwaltung.java
+++ src/Domain/Mitarbeiterverwaltung.java
@@ -1,5 +1,7 @@
 package Domain;
 
+import ValueObjekt.Artikel;
+import ValueObjekt.Kunde;
 import ValueObjekt.Mitarbeiter;
 
 import java.util.ArrayList;
@@ -7,8 +9,19 @@ import java.util.List;
 
 public class Mitarbeiterverwaltung {
     private List<Mitarbeiter> listMitarbeiter = new ArrayList<>();
+    private List<Mitarbeiter> mitarbeiterListe;
+    private List<Artikel> artikelList ;
+    private List<Kunde> kundeListe;
 
     public List<Mitarbeiter> getListMitarbeiter(){
         return listMitarbeiter;
+    }
+
+
+
+
+    public void neuenMitarbeiterRegistrieren(String userName, String passwort, String nachname, String vorname, int nummer, int mitarbeiterNummer) {
+        Mitarbeiter neuerMitarbeiter = new Mitarbeiter(userName, passwort,nachname,vorname,nummer,mitarbeiterNummer);
+        mitarbeiterListe.add(neuerMitarbeiter);
     }
 }
