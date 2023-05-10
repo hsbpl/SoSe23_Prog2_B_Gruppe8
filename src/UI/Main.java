9c8a9f69164a8b54775a9b12defa368f8b1f7a84
kemicha <chakounangkevine@yahoo.fr>
2023-05-10 13:40:42 +0200

    neue Update

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -1,13 +1,14 @@
 package UI;
 
-import Domain.Artikelverwaltung;
-import Domain.Kundenverwaltung;
-import Domain.Lagerverwaltung;
-import Domain.Mitarbeiterverwaltung;
+import Domain.*;
 import ValueObjekt.Artikel;
 import ValueObjekt.Kunde;
 import ValueObjekt.Mitarbeiter;
 import ValueObjekt.User;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -16,31 +17,99 @@ import java.util.Scanner;
 public class Main {
     //fast fertig
 
-    public static void main(String[] args) {
-        //Scanner Klasse für Keyboard-Input
-        //Scanner scanner = new Scanner(System.in);
-
-
-        Lagerverwaltung lagerverwaltung = new Lagerverwaltung();
-
-        // Neue Artikel anlegen
-        lagerverwaltung.neuenArtikelAnlegen("Artikel 1", 10);
-        lagerverwaltung.neuenArtikelAnlegen("Artikel 2", 5);
-
-        // Bestand erhöhen
-        lagerverwaltung.bestandErhoehen("Artikel 1", 20);
-
-        // Neue Mitarbeiter registrieren
-        lagerverwaltung.neuenMitarbeiterRegistrieren("Benutzer1", "Passwort123");
-
-        // usereinloggen
-        Mitarbeiter eingeloggterMitarbeiter = lagerverwaltung.mitarbeiterEinloggen("Benutzer1", "Passwort123");
-        if (eingeloggterMitarbeiter != null) {
-            System.out.println("Erfolgreich eingeloggt: " + eingeloggterMitarbeiter.getUserName());
-        } else;
-
+    public Main() {
+        this.warenkorb = new Warenkorb();
     }
 
+    private void menue() {
+        System.out.println("\nBefehle:");
+        System.out.println("Artikel ausgeben: 'a'");
+        System.out.println("Artikel löschen: 'b'");
+        System.out.println("Artikel einfügen: 'c'");
+        System.out.println("Artikel suchen: 'd'");
+        System.out.println("Warenkorb anzeigen: 'e'");
+        System.out.println("Warenkorb leeren: 'f'");
+        System.out.println("---------------------");
+        System.out.println("Beenden: 'x'");
+        System.out.print("> "); // Prompt
+        System.out.flush(); // ohne new line ausgeben
+    }
+
+    private String liesEingabe() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        return br.readLine();
+    }
+
+    private void verarbeiteEingabe(String input) {
+        switch (input) {
+            case "a":
+                System.out.println(this.warenkorb.getArtikelListe());
+                break;
+            case "b":
+                System.out.print("Artikelnummer: ");
+                try {
+                    int artikelnummer = Integer.parseInt(liesEingabe());
+                    this.warenkorb.removeArtikel(artikelnummer);
+                    System.out.println("Artikel gelöscht.");
+                } catch (NumberFormatException e) {
+                    System.out.println("Ungültige Eingabe!");
+                }
+                break;
+            case "c":
+                System.out.print("Artikelname: ");
+                String name = "";
+                try {
+                    name = liesEingabe();
+                } catch (IOException e1) {
+                    System.out.println("Fehler beim Einlesen des Namens!");
+                    break;
+                }
+
+                System.out.print("Artikelnummer: ");
+                int artikelnummer = 0;
+                try {
+                    artikelnummer = Integer.parseInt(liesEingabe());
+                } catch (NumberFormatException e) {
+                    System.out.println("Ungültige Eingabe!");
+                    break;
+                }
+
+                System.out.print("Preis: ");
+                double preis = 0;
+                try {
+                    preis = Double.parseDouble(liesEingabe());
+                } catch (NumberFormatException e) {
+                    System.out.println("Ungültige Eingabe!");
+                    break;
+                }
+
+                Artikel artikel = new Artikel(, artikelnummer, preis);
+                this.warenkorb.addArtikel(artikel);
+                System.out.println("Artikel hinzugefügt.");
+                break;
+            case "d":
+                System.out.print("Suchbegriff: ");
+                String suchbegriff = "";
+                try {
+                    suchbegriff = liesEingabe();
+                } catch (IOException e1) {
+                    System.out.println("Fehler beim Einlesen des Suchbegriffs!");
+                    break;
+                }
+                System.out.println(this.warenkorb.getWarenkorb());
+                break;
+            case "e":
+                System.out.println(this.warenkorb.getWarenkorb());
+                break;
+            case "f":
+                this.warenkorb.leereWarenkorb();
+                System.out.println("Warenkorb geleert.");
+                break;
+            case "x":
+
+
+        }
+    }
 }
 
 
