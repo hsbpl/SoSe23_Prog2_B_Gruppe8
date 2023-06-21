47d18b1b42b9a7e5df052a45b034c25bdbb9b377
RohaAhmad <rahmad@stud.hs-bremen.de>
2023-06-21 03:12:27 +0200

    Ereignisklasse überarbeitet und korrektes Format für Datum eingesetzt

--- src/ValueObjekt/Ereignis.java
+++ src/ValueObjekt/Ereignis.java
@@ -1,17 +1,22 @@
 package ValueObjekt;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class Ereignis {
     // Datum als Atribut
-    private Date datum;
+    //private Date datum = new Date();
+    //private LocalDateTime zeitstempel_orig = LocalDateTime.now();
+    //private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+    //private String zeitstempel_string = zeitstempel_orig.format(formatter);
+    private LocalDateTime datum;
     private int anzahl;
     private Artikel artikel;
     private User user;
     private Enum ereignistyp;
 
-    private SimpleDateFormat format;
     private int aktualisierterBestand;
 
 
@@ -21,8 +26,16 @@ public class Ereignis {
         this.user = user;
         this.ereignistyp = ereignistyp;
         this.aktualisierterBestand = aktualisierterBestand;
-        this.datum = new Date();
-        this.format = new SimpleDateFormat("MMMM/dd/Y HH:mm");
+        this.datum = LocalDateTime.now();
+    }
+
+    public Ereignis( int anzahl, Artikel artikel, User user, Enum ereignistyp, int aktualisierterBestand, LocalDateTime zeitstempel) {
+        this.anzahl = anzahl;
+        this.artikel = artikel;
+        this.user = user;
+        this.ereignistyp = ereignistyp;
+        this.aktualisierterBestand = aktualisierterBestand;
+        this.datum = zeitstempel;
     }
     public int getAnzahl(){return anzahl;}
     public Artikel getArtikel(){return artikel;}
@@ -30,20 +43,16 @@ public class Ereignis {
     public Enum getEreignistyp(){return ereignistyp;}
     public int getAktualisierterBestand(){return aktualisierterBestand;}
 
-    public Date getDatum(){
+    public LocalDateTime getDatum(){
         return datum;
     }
 
     //TODO Bestand wird immer Aktuallisiert, statt das er den alten Bestand anzeigt, überlegen ob das abgeändert werden müsste)
     public String toString(){
-        return  format.format(datum) + "\n"+
+        return  getDatum().toString() + "\n"+
                 anzahl +" durchgeführt über " + ereignistyp.toString() +"; "+
                 user + "Aktueller Bestand: " + artikel.getBezeichnung()+ " \n"+
                 "Aktuellster Stand: " + aktualisierterBestand
                 + "\n";
     }
 }
-
-
-
-
