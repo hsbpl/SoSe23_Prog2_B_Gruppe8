0188a27e38022efd0c2a92ad500c4b84dd3d2dfc
RohaAhmad <rahmad@stud.hs-bremen.de>
2023-06-23 19:04:24 +0200

    JListe zur JTable umgeändert damit der Kunde nur den Artikel und Preis sieht. Man kann nun Artikel in den Warenkorb hinzufügen und auch ansehen, was man hinzugefügt hat.

--- src/UI/KundenbereichGUI.java
+++ src/UI/KundenbereichGUI.java
@@ -8,12 +8,12 @@ import ValueObjekt.Warenkorb;
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.Map;
-
 
 public class KundenbereichGUI extends JFrame {
     private EShop eShop;
@@ -24,7 +24,6 @@ public class KundenbereichGUI extends JFrame {
         super("Kundenbereich");
         this.eingeloggterKunde = eingeloggterKunde;
         this.warenKorbDesKunden = warenKorbDesKunden;
-        //String datei = "ESHOP";
         this.eShop = eShop;
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -44,135 +43,102 @@ public class KundenbereichGUI extends JFrame {
         JButton artikelEntfernenButton = new JButton("Artikel entfernen");
         JButton warenkorbButton = new JButton("Warenkorb anzeigen");
         JButton warenkorbLeerenButton = new JButton("Warenkorb leeren");
-        DefaultListModel<String> artikelListModel = new DefaultListModel<>();
-        JList<String> artikelList = new JList<>(artikelListModel);
-        JScrollPane artikelScrollPane = new JScrollPane(artikelList);
 
-        // Artikel-Liste
-        artikelListModel = new DefaultListModel<>();
-        artikelList = new JList<>(artikelListModel);
-        artikelScrollPane = new JScrollPane(artikelList);
-        artikelScrollPane.setPreferredSize(new Dimension(300, getHeight()));
+        DefaultTableModel artikelTableModel = new DefaultTableModel();
+        JTable artikelTable = new JTable(artikelTableModel);
+        JScrollPane artikelScrollPane = new JScrollPane(artikelTable);
 
-        // Erstellen der JPanel-Komponente für den Scrollbereich
-        JPanel artikelPanel = new JPanel(new BorderLayout());
-        artikelPanel.add(artikelScrollPane, BorderLayout.CENTER);
+        // Artikel-Tabelle konfigurieren
+        artikelTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        artikelTable.setRowSelectionAllowed(true);
 
-        // Hinzufügen des ListSelectionListener für die ArtikelListe
-        JList<String> finalArtikelList1 = artikelList;
-        DefaultListModel<String> finalArtikelListModel2 = artikelListModel;
-        artikelList.addListSelectionListener(new ListSelectionListener() {
-
-
-            //TODO ich wollte dir nichts kaputt machen hier habe ich die Artikelliste als Jtable hingelegt kannst ihn statt der Liste verwenden
-            private JTable tabelle;
-            private ArtikelTableModel model;
-            private JScrollPane tablePane;
-            private Component artikellistTable(){
-                tabelle = new JTable();
-                model =new ArtikelTableModel(eShop.getAlleArtikel());
-                tabelle.setModel(new ArtikelTableModel(eShop.getAlleArtikel()));
-                tablePane = new JScrollPane(tabelle);
-
-                return tablePane;
-            }
-
+        // Hinzufügen des ListSelectionListeners für die Artikel-Tabelle
+        artikelTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
             @Override
             public void valueChanged(ListSelectionEvent e) {
                 if (!e.getValueIsAdjusting()) {
-                    // Abfragen, ob der ausgewählte Artikel zum Warenkorb hinzugefügt werden soll
-                    int selectedIndex = finalArtikelList1.getSelectedIndex();
-                    if (selectedIndex != -1) {
-                        String selectedArtikel = finalArtikelListModel2.getElementAt(selectedIndex);
+                    int selectedRow = artikelTable.getSelectedRow();
+                    if (selectedRow != -1) {
+                        String selectedArtikel = (String) artikelTableModel.getValueAt(selectedRow, 0);
                         int option = JOptionPane.showConfirmDialog(null, "Möchten Sie den Artikel\n" + selectedArtikel + "\nzum Warenkorb hinzufügen?", "Artikel zum Warenkorb hinzufügen", JOptionPane.YES_NO_OPTION);
                         if (option == JOptionPane.YES_OPTION) {
-                            // Artikel zum Warenkorb hinzufügen
-                            String artikel = eShop.artikelListen();
-                            warenKorbDesKunden.getWarenkorb();
+                            Artikel artikel = eShop.getAlleArtikel().get(selectedRow);
+                            warenKorbDesKunden.getWarenkorb().put(artikel, 1);
                             JOptionPane.showMessageDialog(null, "Artikel erfolgreich zum Warenkorb hinzugefügt.");
                         }
-                        // Auswahl in der ArtikelListe zurücksetzen
-                        finalArtikelList1.clearSelection();
+                        artikelTable.clearSelection();
                     }
                 }
             }
         });
 
-        // Laden aller Artikel aus dem EShop
+        // Artikel-Tabelle modellieren
+        artikelTableModel.addColumn("Artikel");
+        artikelTableModel.addColumn("Preis");
+
         for (Artikel artikel : eShop.getAlleArtikel()) {
-            artikelListModel.addElement(artikel.toString());
+            artikelTableModel.addRow(new Object[]{artikel.getBezeichnung(),artikel.getEinzelpreis()});
         }
 
-        // Konfigurieren der GUI-Komponenten
         titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
+
+        // ActionListener für Ausloggen-Button
         logoutButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 int option = JOptionPane.showConfirmDialog(null, "Möchten Sie sich wirklich ausloggen?", "Ausloggen", JOptionPane.YES_NO_OPTION);
                 if (option == JOptionPane.YES_OPTION) {
-
-                        StartGUI s = new StartGUI(eShop);
-                        dispose();
-
+                    StartGUI s = new StartGUI(eShop);
+                    dispose();
                 }
             }
         });
 
-        infoPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 10));
-
-        infoPanel.add(titleLabel);
-
-        // Hinzufügen der ActionListener für den "Artikel kaufen"-Button
+        // ActionListener für Artikel kaufen-Button
         artikelKaufenButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                // Implementiere hier deine Logik für den Artikel kaufen-Button
             }
         });
 
-        // Hinzufügen des ActionListener für den "Artikel entfernen"-Button
-        JList<String> finalArtikelList = artikelList;
-        DefaultListModel<String> finalArtikelListModel = artikelListModel;
+        // ActionListener für Artikel entfernen-Button
         artikelEntfernenButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Ausgewählten Artikel aus der Liste entfernen
-                int selectedIndex = finalArtikelList.getSelectedIndex();
-                if (selectedIndex != -1) {
-                    finalArtikelListModel.remove(selectedIndex);
+                int selectedRow = artikelTable.getSelectedRow();
+                if (selectedRow != -1) {
+                    artikelTableModel.removeRow(selectedRow);
                 }
             }
         });
 
-        // Hinzufügen des ActionListener für den "Warenkorb anzeigen"-Button
+        // ActionListener für Warenkorb anzeigen-Button
         warenkorbButton.addActionListener(new ActionListener() {
-
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Dialogbox zum Anzeigen des Warenkorbs anzeigen
                 StringBuilder warenkorbText = new StringBuilder();
                 for (Map.Entry<Artikel, Integer> eintrag : warenKorbDesKunden.getWarenkorb().entrySet()) {
                     Artikel artikel = eintrag.getKey();
                     int menge = eintrag.getValue();
                     warenkorbText.append(artikel.toString()).append(" - Menge: ").append(menge).append("\n");
                 }
-
                 JOptionPane.showMessageDialog(null, warenkorbText.toString(), "Warenkorb anzeigen", JOptionPane.INFORMATION_MESSAGE);
             }
         });
 
-        // Hinzufügen des ActionListener für den "Warenkorb leeren"-Button
-        DefaultListModel<String> finalArtikelListModel1 = artikelListModel;
+        // ActionListener für Warenkorb leeren-Button
         warenkorbLeerenButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Warenkorb leeren
                 warenKorbDesKunden.getWarenkorb().clear();
-                finalArtikelListModel1.clear();
+                artikelTableModel.setRowCount(0);
             }
         });
 
-        // Zusammenstellen der GUI
+        infoPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 10));
+        infoPanel.add(titleLabel);
+
         buttonPanel.add(logoutButton);
         buttonPanel.add(artikelKaufenButton);
         buttonPanel.add(artikelEntfernenButton);
@@ -180,7 +146,7 @@ public class KundenbereichGUI extends JFrame {
         buttonPanel.add(warenkorbLeerenButton);
 
         mainPanel.add(infoPanel, BorderLayout.NORTH);
-        mainPanel.add(artikelPanel, BorderLayout.WEST); // Artikel-Liste links anzeigen
+        mainPanel.add(artikelScrollPane, BorderLayout.WEST);
         mainPanel.add(buttonPanel, BorderLayout.SOUTH);
 
         this.add(mainPanel);
@@ -188,17 +154,8 @@ public class KundenbereichGUI extends JFrame {
 
     private void kundenbereich() {
     }
-
-   /* public static void main(String[] args) {
-        try {
-            KundenbereichGUI kundenbereichGUI = new KundenbereichGUI(new Kunde("roha","1234", "ahmad", "roha", "Berlinerstraße"), new Warenkorb());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    */
 }
+
 
 
 
