a5b12951725947eb181862036989b48a34089dec
RohaAhmad <rahmad@stud.hs-bremen.de>
2023-07-03 20:20:33 +0200

    Mit den menus angefangen

---
+++ src/menus/FileMenu.java
@@ -0,0 +1,61 @@
+package menus;
+
+import Domain.EShop;
+import UI.StartGUI;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+public class FileMenu extends JMenu implements ActionListener {
+
+    private JFrame frame;
+    private EShop eShop;
+
+    public FileMenu(JFrame frame, EShop eShop) {
+        super("File");
+
+        this.frame = frame;
+        this.eShop =eShop;
+
+        JMenuItem mi = new JMenuItem("Sichern");
+        //mi.setEnabled(false);
+        mi.addActionListener(this);
+        this.add(mi);
+
+        this.addSeparator();
+
+        mi = new JMenuItem("Abbrechen");
+        mi.addActionListener(this);
+        this.add(mi);
+
+        this.addSeparator();
+
+        mi = new JMenuItem("Ausloggen");
+        mi.addActionListener(this);
+        this.add(mi);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        String command = ae.getActionCommand();
+        System.out.println(command);
+
+        if (command.equals("Sichern")) {
+            try {
+                eShop.schreibeArtikel();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else if (command.equals("Abbrechen")) {
+            frame.setVisible(false);
+            frame.dispose();
+            System.exit(0);
+        } else if (command.equals("Ausloggen")) {
+            frame.setVisible(false);
+            frame.dispose();
+            StartGUI eShop = new StartGUI( this.eShop);
+        }
+    }
+}
---
+++ src/menus/HelpMenu.java
@@ -0,0 +1,26 @@
+package menus;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class HelpMenu extends JMenu implements ActionListener {
+
+    public HelpMenu() {
+        super("Hilfe");
+
+        JMenuItem miP = new JMenuItem("Programmierer");
+        miP.addActionListener(this);
+        add(miP);
+        //m.addActionListener(this);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getActionCommand()== "Programmers") {
+            JOptionPane.showMessageDialog(null, "Dieses Programm wurde Programmiert von Roha und Sanjana bei Problemen melden sie sich bei den beiden","Programmierer",1);
+
+        }
+
+        System.out.println("Klick auf Menu '" + e.getActionCommand() + "'.");
+    }
+}
+
