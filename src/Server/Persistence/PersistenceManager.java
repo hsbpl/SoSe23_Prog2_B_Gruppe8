d9320d762d51a14e5f1e24bf732ae4e7de046833
RohaAhmad <rahmad@stud.hs-bremen.de>
2023-07-05 14:48:17 +0200

    Die publics entfernt und todo eingefügt

--- src/Server/Persistence/PersistenceManager.java
+++ src/Server/Persistence/PersistenceManager.java
@@ -14,22 +14,22 @@ import java.util.HashMap;
 import java.util.List;
 
 public interface PersistenceManager {
+     // todo HashMap zur Map machen
 
-    public List<Artikel> leseArtikelListe(String datei) throws IOException, ArtikelExistiertBereitsException;
-    public void schreibeArtikelListe(List<Artikel> liste, String datei) throws IOException;
+     List<Artikel> leseArtikelListe(String datei) throws IOException, ArtikelExistiertBereitsException;
+     void schreibeArtikelListe(List<Artikel> liste, String datei) throws IOException;
 
-    public HashMap<String, Kunde> leseKundenListe(String datei) throws IOException, UserExistiertBereitsException;
-    public void schreibeKundeListe(List<Kunde> liste, String datei) throws IOException;
+     HashMap<String, Kunde> leseKundenListe(String datei) throws IOException, UserExistiertBereitsException;
+     void schreibeKundeListe(List<Kunde> liste, String datei) throws IOException;
 
-    public List<Mitarbeiter> leseMitarbeiterList(String datei) throws IOException, UserExistiertBereitsException;
-    public void schreibeMitarbeiterListe(List<Mitarbeiter> liste, String datei) throws IOException;
+     List<Mitarbeiter> leseMitarbeiterList(String datei) throws IOException, UserExistiertBereitsException;
+     void schreibeMitarbeiterListe(List<Mitarbeiter> liste, String datei) throws IOException;
 
+     List<Ereignis> leseEreignisList(String datei) throws IOException, EreignisExistiertBereitsException;
+     void schreibeEreignisListe(List<Ereignis> liste, String datei) throws IOException;
 
-    public List<Ereignis> leseEreignisList(String datei) throws IOException, EreignisExistiertBereitsException;
-    public void schreibeEreignisListe(List<Ereignis> liste, String datei) throws IOException;
-
-    public List<Artikel> leseMassengutListe(String datei) throws IOException, EreignisExistiertBereitsException;
-    public void schreibeMassengutListe(List<Artikel> liste, String datei) throws IOException;
+     List<Artikel> leseMassengutListe(String datei) throws IOException, EreignisExistiertBereitsException;
+     void schreibeMassengutListe(List<Artikel> liste, String datei) throws IOException;
     }
 
 
